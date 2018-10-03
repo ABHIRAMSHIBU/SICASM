babcd07174795834e3891fe278f5139f56770272
ABHIRAMSHIBU <abhiramshibu1998@gmail.com>
2018-10-03 07:11:38 +0530

    File Handler Partial

--- Assembler.cpp
+++ Assembler.cpp
@@ -8,6 +8,7 @@
 #include<iostream>
 #include<map>
 #include<functional>
+#include<fstream>
 using namespace std;
 typedef function<void(string,int)> fun;
 class Table{
@@ -64,10 +65,98 @@ public:
 		OPTAB["WD"]="DC";
 	}
 };
+class fileHandler{
+private:
+	fstream f;
+	bool backEn;
+	int prevLocation;
+public:
+	bool eof;
+	int location;
+	int line;
+	fileHandler(string name,char operation){
+		if(operation=='r'){
+			f.open(name.c_str(),ios::in);
+		}
+		if(operation=='w'){
+			f.open(name.c_str(),ios::out);
+		}
+		eof=f.eof();
+		location=f.tellg();
+		line=0;
+		prevLocation=0;
+		backEn=true;
+	}
+	string readLine(){
+		char buff[1];
+		string data="";
+		while(!f.eof() && buff[0]!='\n'){
+			f.read(buff,1);
+			data+=buff[0];
+		}
+		prevLocation=location;
+		location=f.tellg();
+		eof=f.eof();
+		line++;
+		backEn=true;
+		return data;
+	}
+	void print(string data){
+		f.write(data.c_str(),data.length());
+	}
+	void write(string data){
+		print(data);
+	}
+	void println(string data){
+		f.write((data+"\n").c_str(),(data+"\n").length());
+	}
+	void writeln(string data){
+		println(data);
+	}
+	string front(){
+		return readLine();
+	}
+	string next(){
+		return readLine();
+	}
+	bool previous(){
+		return back();
+	}
+	bool back(){
+		if(backEn){
+			f.seekg(prevLocation);
+			return 0;
+		}
+		else{
+			return 1;
+		}
+		backEn=false;
+	}
+	void seekLine(int lineNo){
+		f.seekg(0);
+		for(int i=lineNo;i>0;i++){
+			readLine();
+		}
+	}
+};
+class processASM{
+	string fileName;
+public:
+	processASM(string fileName){
+		this->fileName=fileName;
+	}
+	static void process(processASM p){
+		fileHandler f(p.fileName,'r');
+		cout<<f.readLine();
+	}
+};
 int main(int argc, char **argv) {
 	Table t;
 	t.init();
-	t.PSEUDOOPTAB["RESW"]("Meh",1);
+	if(argc>1){
+		processASM p(argv[1]);
+		processASM::process(p);
+	}
 }
 
 
